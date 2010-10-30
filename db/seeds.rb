aff7df186facdc972756dc8e76fa19fc5e306e3b
hebelken <hebelken@gmail.com>
2010-10-30 01:19:41 -0700

    multple posts display cdorrectly

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,17 @@
 
 authors = Author.create([{ :name => "hebelken"}, { :name => "timmy"}, { :name => "barny Stinson"}])
 
-posts = Post.create([{ :author => authors.first, :rating => 5, :body => "yo tina, I'mma let you finish but jeffry has the..."}])
+posts = Post.create([
+  { :author => authors.first, 
+    :rating => 5, 
+    :body => "yo tina, I'mma let you finish but jeffry has the..."},
+  { :author => authors.first, 
+    :rating => 1, 
+    :body => "herp-derp dee deed derp"},
+  { :author => authors.last, 
+    :rating => 2, 
+    :body => "this is the name that is mine my post is baller and I look so fine"},
+  { :author => authors[1], 
+    :rating => 5, 
+    :body => "this is alebeeb psors dort aohtem lariousemsasdgnm ipsotmsatmm"
+}])
