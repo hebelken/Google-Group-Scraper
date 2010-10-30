9a99dae4fe979c2801e8269560b84f32b13f4953
hebelken <hebelken@gmail.com>
2010-10-30 00:30:17 -0700

    Authors working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+authors = Author.create([{ :name => "hebelken"}, { :name => "timmy"}, { :name => "barny Stinson"}])
